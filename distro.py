9ac5a27b8805bdabffb4b16753f4c07627996964
njhartfo <71589945+njhartfo@users.noreply.github.com>
2021-07-15 02:33:52 -0500

    Create distro.py

---
+++ distro.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+__project__ = ""
+__version__ = "0.0.1"
+__description__ = ""
+__packages__ = [""]
+__author__ = "Nathan Hartford"
+__classifiers__ = [
+    "Development Status :: 3 - Alpha",
+    "Intended Audience :: Personal",
+    "Programming Language :: Python :: 3",
+]
+__requires__ = ["package name here"]
+
+setup(
+    name = __project__,
+    version = __version__,
+    description = __description__,
+    packages = __packages__,
+    author = __author__,
+    classifiers = __classifiers__,
+    requires = __requires__
+)
